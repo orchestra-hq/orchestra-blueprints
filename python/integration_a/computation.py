05f0913cd5c2a503ac78df086af87ba8c1001fbf
Hugo Lu <127905842+hugolu-1@users.noreply.github.com>
2025-04-02 16:00:17 +0100

    Update computation.py

--- python/integration_a/computation.py
+++ python/integration_a/computation.py
@@ -40,8 +40,6 @@ data = {
 
 url = "https://webhook.getorchestra.io"
 api_key = os.getenv('ORCHESTRA_API_KEY')
-print(api_key[:-3])
-print(os.getenv('ORCHESTRA_TASK_RUN_ID'))
 # Set headers with Bearer authentication
 headers = {
     "Content-Type": "application/json",
