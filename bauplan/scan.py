0c44cd5b0c4e137a5a7c0e97861cae8fcab811a4
Oliver Crawford <oliver@getorchestra.io>
2025-07-25 16:29:45 +0100

    test bauplan in Orchestra

--- bauplan/scan.py
+++ bauplan/scan.py
@@ -3,24 +3,24 @@ from datetime import datetime
 import bauplan
 import os
 
+
 def source_to_iceberg_table(
     bauplan_client: bauplan.Client,
     table_name: str,
     namespace: str,
     source_s3_pattern: str,
-    bauplan_ingestion_branch: str
+    bauplan_ingestion_branch: str,
 ):
     """
-    
+
     Wrap the table creation and upload process in Bauplan.
-    
+
     """
     if bauplan_client.has_branch(bauplan_ingestion_branch):
         bauplan_client.delete_branch(bauplan_ingestion_branch)
-        
-    
+
     # create the branch from main HEAD
-    bauplan_client.create_branch(bauplan_ingestion_branch, from_ref='main')
+    bauplan_client.create_branch(bauplan_ingestion_branch, from_ref="main")
     # we check if the branch is there (and learn a new API method ;-))
     assert bauplan_client.has_branch(bauplan_ingestion_branch), "Branch not found"
     # now we create the table in the branch
@@ -30,16 +30,18 @@ def source_to_iceberg_table(
         namespace=namespace,
         branch=bauplan_ingestion_branch,
         # just in case the test table is already there for other reasons
-        replace=True  
+        replace=True,
     )
     # we check if the table is there (and learn a new API method ;-))
     fq_name = f"{namespace}.{table_name}"
-    assert bauplan_client.has_table(table=fq_name, ref=bauplan_ingestion_branch), "Table not found"
+    assert bauplan_client.has_table(table=fq_name, ref=bauplan_ingestion_branch), (
+        "Table not found"
+    )
     is_imported = bauplan_client.import_data(
         table=table_name,
         search_uri=source_s3_pattern,
         namespace=namespace,
-        branch=bauplan_ingestion_branch
+        branch=bauplan_ingestion_branch,
     )
 
     return is_imported
@@ -49,107 +51,99 @@ def run_quality_checks(
     bauplan_client: bauplan.Client,
     bauplan_ingestion_branch: str,
     table_name: str,
-    namespace: str
+    namespace: str,
 ):
     """
-    
-    We check the data quality by running the checks in-process: we use 
-    Bauplan SDK to query the data as an Arrow table, and check if the 
+
+    We check the data quality by running the checks in-process: we use
+    Bauplan SDK to query the data as an Arrow table, and check if the
     target column is not null through vectorized PyArrow operations.
-    
+
     """
     # we retrieve the data and check if the table is column has any nulls
     # make sure the column you're checking is in the table, so change this appropriately
     # if you're using a different dataset
-    column_to_check = 'PULocationID'
+    column_to_check = "PULocationID"
     # NOTE if you don't want to use any SQL, you can interact with the lakehouse in pure Python
     # and still back an Arrow table (in this one column) through a performant scan.
     print("Perform a S3 columnar scan on the column {}".format(column_to_check))
     wap_table = bauplan_client.scan(
-        table=namespace+"."+table_name,
+        table=namespace + "." + table_name,
         ref=bauplan_ingestion_branch,
-        columns=[column_to_check]
+        columns=[column_to_check],
     )
-    #print("Read the table successfully!")
-    #assert wap_table[column_to_check].null_count > 0, "Quality check failed"
-    #print("Quality check passed")
-    
+    # print("Read the table successfully!")
+    # assert wap_table[column_to_check].null_count > 0, "Quality check failed"
+    # print("Quality check passed")
+
     return True
 
 
-def merge_branch(
-    bauplan_client: bauplan.Client,
-    bauplan_ingestion_branch: str
-):
+def merge_branch(bauplan_client: bauplan.Client, bauplan_ingestion_branch: str):
     """
-    
+
     We merge the ingestion branch into the main branch. If this succeed,
     the transaction itself is considered successful.
-    
+
     """
     # we merge the branch into the main branch
     return bauplan_client.merge_branch(
-        source_ref=bauplan_ingestion_branch,
-        into_branch='main'
+        source_ref=bauplan_ingestion_branch, into_branch="main"
     )
 
 
 def delete_branch_if_exists(transaction):
     """
-    
+
     If the task fails or the merge succeeded, we delete the branch to avoid clutter!
-    
+
     """
     _client = bauplan.Client()
-    ingestion_branch = transaction.get('bauplan_ingestion_branch')
-    if  _client.has_branch(ingestion_branch):
+    ingestion_branch = transaction.get("bauplan_ingestion_branch")
+    if _client.has_branch(ingestion_branch):
         print(f"Deleting the branch {ingestion_branch}")
         _client.delete_branch(ingestion_branch)
     else:
         print(f"Branch {ingestion_branch} does not exist, nothing to delete.")
-        
+
     return
 
 
 def wap_with_bauplan(
-    bauplan_ingestion_branch: str, 
+    bauplan_ingestion_branch: str,
     source_s3_pattern: str,
     table_name: str,
-    namespace: str
+    namespace: str,
 ):
     """
     Run the WAP ingestion pipeline using Bauplan in a Prefect flow
     leveraging the new concept of transactions:
-    
+
     https://docs-3.prefect.io/3.0rc/develop/transactions#write-your-first-transaction
     """
     print("Starting WAP at {}!".format(datetime.now()))
-    bauplan_client = bauplan.Client(api_key=os.environ['BAUPLAN_API_KEY'])
+    bauplan_client = bauplan.Client(api_key=os.environ["BAUPLAN_API_KEY"])
     ### THIS IS THE WRITE
     # first, ingest data from the s3 source into a table the Bauplan branch
     source_to_iceberg_table(
         bauplan_client,
-        table_name, 
+        table_name,
         namespace,
         source_s3_pattern,
-        bauplan_ingestion_branch
+        bauplan_ingestion_branch,
     )
     ### THIS IS THE AUDIT
     # we query the table in the branch and check we have no nulls
     run_quality_checks(
         bauplan_client,
-        bauplan_ingestion_branch, 
+        bauplan_ingestion_branch,
         table_name=table_name,
-        namespace=namespace
+        namespace=namespace,
     )
-    # THIS IS THE PUBLISH 
+    # THIS IS THE PUBLISH
     # finally, we merge the branch into the main branch if the quality checks passed
-    merge_branch(
-        bauplan_client,
-        bauplan_ingestion_branch
-    )
-    
-            
+    merge_branch(bauplan_client, bauplan_ingestion_branch)
+
     # say goodbye
     print("All done at {}, see you, space cowboy.".format(datetime.now()))
 
@@ -159,13 +153,11 @@ def wap_with_bauplan(
 if __name__ == "__main__":
     print("Starting WAP at {}!".format(datetime.now()))
 
-    bauplan_client = bauplan.Client(api_key=os.environ['BAUPLAN_API_KEY'])
-    
-    #ghyjkl
+    bauplan_client = bauplan.Client(api_key=os.environ["BAUPLAN_API_KEY"])
+
+    # ghyjkl
     return_object = bauplan_client.run(ref="orchestra.dev_hugo", namespace="bauplan")
     job_list = bauplan_client.get_job_logs(job_id_prefix=return_object.job_id)
     for i in job_list:
         print(i.message)
     print("Data Transformations run successfully")
-
-  
--- hybrid_deploy/main.tf
+++ hybrid_deploy/main.tf
@@ -12,13 +12,19 @@ provider "aws" {
 }
 
 module "ecs-compute" {
-  source = "orchestra-hq/ecs-compute/aws"
+  # source = "../../terraform-aws-ecs-compute"
+  source  = "orchestra-hq/ecs-compute/aws"
+  version = "1.0.0"
 
   # REPLACE MY VALUES
-  region               = "eu-west-2"
-  name_prefix          = "name-prefix"
-  orchestra_account_id = "YOUR_ACCOUNT_ID"
-  integrations         = ["python", "dbt_core"]
+  region                   = "eu-west-2"
+  name_prefix              = "test-v6"
+  orchestra_account_id     = "84e75049-b4c3-4a93-a595-21cff92bdb9d"
+  orchestra_aws_account_id = "383742555833"
+  integrations             = ["python"]
+  image_tags = {
+    "python" : "54896a6c885f73ae629e4390cc2361127f409f9d"
+  }
 }
 
 data "aws_security_groups" "security_groups" {}
