699f57c601966983e8f57df6f79a6f1ffa0dfc87
hugolu-1 <hugo@getorchestra.io>
2025-09-23 15:23:22 +0100

    Update main script

--- dlt/run_dlt_pipelines.py
+++ dlt/run_dlt_pipelines.py
@@ -7,20 +7,20 @@ try:
     sheet_name = os.getenv('SHEET_NAME')
 except:
     #sheet_name = '1H0UnZ1vJ6WSsZgiVkg96zq52p7qaXkhvodlO1Mzoj6s'
-    sheet_name = '1TMNhQFZbmaBsa1vIehQzh3vdyfc05damwJ_PIKDF14A'
+    sheet_name = None
 try:
     range_name = os.getenv('RANGE_NAME')
 except:
-    range_name = 'dlt_range'
+    range_name = None
 try:
-    table_name = os.getenv('TABLE_NAME')
+    table_name = os.getenv('TABLE_NAME') or "dbt_leads"
 except:
-    table_name = 'test'
+    table_name = "test"
 try:
-    drop_sources = os.getenv('REFRESH_MODE')
+    drop_sources = os.getenv('REFRESH_MODE') 
 except:
     drop_sources = None
 
-run_google_sheets_pipeline(sheet_name, range_names=range_name, drop_mode = "drop_data", table_name="dbt_leads_v2")
+run_google_sheets_pipeline(sheet_name, range_names=range_name, drop_mode = drop_sources, table_name=table_name)
 #run_pipeline()
 print("Success")
