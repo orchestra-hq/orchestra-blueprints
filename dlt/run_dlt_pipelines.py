d0339ad88730c1ccf67f40f8aa636071ef9270fb
hugolu-1 <hugo@getorchestra.io>
2025-09-23 08:42:50 +0100

    Add dlt for google sheets

--- dlt/run_dlt_pipelines.py
+++ dlt/run_dlt_pipelines.py
@@ -1,6 +1,18 @@
 from hubspot_pipeline import run_pipeline
+from google_sheets_pipeline import run_google_sheets_pipeline
 import os
 
 
-run_pipeline()
+try:
+    sheet_name = os.getenv('SHEET_NAME')
+except:
+    sheet_name = '1H0UnZ1vJ6WSsZgiVkg96zq52p7qaXkhvodlO1Mzoj6s'
+try:
+    range_name = os.getenv('RANGE_NAME')
+except:
+    range_name = 'dlt_range'
+
+
+run_google_sheets_pipeline(sheet_name, range_names=range_name, drop_mode = None)
+#run_pipeline()
 print("Success")
