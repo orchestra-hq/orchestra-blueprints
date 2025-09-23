107d11cad1b3285089c4ced7ff68f1c46813087b
hugolu-1 <hugo@getorchestra.io>
2025-09-23 14:56:33 +0100

    Ading back config

--- dlt/run_dlt_pipelines.py
+++ dlt/run_dlt_pipelines.py
@@ -6,7 +6,8 @@ import os
 try:
     sheet_name = os.getenv('SHEET_NAME')
 except:
-    sheet_name = '1H0UnZ1vJ6WSsZgiVkg96zq52p7qaXkhvodlO1Mzoj6s'
+    #sheet_name = '1H0UnZ1vJ6WSsZgiVkg96zq52p7qaXkhvodlO1Mzoj6s'
+    sheet_name = '1TMNhQFZbmaBsa1vIehQzh3vdyfc05damwJ_PIKDF14A'
 try:
     range_name = os.getenv('RANGE_NAME')
 except:
@@ -14,9 +15,9 @@ except:
 try:
     dataset_name = os.getenv('DATASET_NAME')
 except:
-    dataset_name = 'dbt_leads'
+    dataset_name = 'sample_google_sheet_data'
 
-
-run_google_sheets_pipeline(sheet_name, range_names=range_name, drop_mode = None, dataset_name=dataset_name)
+print("Sheet name:" + str(sheet_name))
+run_google_sheets_pipeline(sheet_name, range_names=range_name, drop_mode = None, dataset_name="dbt_leads")
 #run_pipeline()
 print("Success")
