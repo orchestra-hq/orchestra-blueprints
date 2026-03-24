c35b9d33f127c17610293c913dc1c9da819cc82b
hugolu-1 <hugo@getorchestra.io>
2026-03-24 09:29:56 +0000

    Add col

--- dbt_projects/snowflake/models/clean/oil_data_clean.sql
+++ dbt_projects/snowflake/models/clean/oil_data_clean.sql
@@ -3,7 +3,7 @@ select
 *,
 sha2_binary(concat(cast(timestamp as string), site_name)) _pk,
 "test_column" as _test_column,
-"additional_column" as _additional_column,
+"additional_column" as broken_column,
 "third column" as _third_column
 
 from {{source('fivetran', 'oil_data')}}
