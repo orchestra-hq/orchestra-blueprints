a3baedc94dff8d6bb3ab361882e02a85fcd950cc
hugolu-1 <hugo@getorchestra.io>
2026-03-24 09:14:33 +0000

    tst change

--- dbt_projects/snowflake/models/clean/oil_data_clean.sql
+++ dbt_projects/snowflake/models/clean/oil_data_clean.sql
@@ -1,6 +1,7 @@
 select
 
 *,
-sha2_binary(concat(cast(timestamp as string), site_name)) _pk
+sha2_binary(concat(cast(timestamp as string), site_name)) _pk,
+"test_column" as _test_column
 
 from {{source('fivetran', 'oil_data')}}
