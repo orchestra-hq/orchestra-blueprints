68df866a11339b05f1ccf677549098b08ea27b8b
hugolu-1 <hugo@getorchestra.io>
2026-04-14 18:46:40 +0100

    Adding the Snowflake Things

---
+++ dlt/run_dlt_pipelines_snowflake.py
@@ -0,0 +1,31 @@
+from hubspot_pipeline import run_pipeline
+from snowflake_pipeline import run_gsheet_snowflake_pipeline
+import os
+
+
+try:
+    sheet_name = os.getenv('SHEET_NAME')
+except:
+    #sheet_name = '1H0UnZ1vJ6WSsZgiVkg96zq52p7qaXkhvodlO1Mzoj6s'
+    sheet_name = None
+try:
+    range_name = os.getenv('RANGE_NAME')
+except:
+    range_name = None
+try:
+    table_name = os.getenv('TABLE_NAME')
+except:
+    table_name = None
+try:
+    drop_sources = os.getenv('REFRESH_MODE') 
+except:
+    drop_sources = None
+
+try:
+
+    run_gsheet_snowflake_pipeline(sheet_name, range_names=range_name, drop_mode = drop_sources, table_name=table_name)
+except Exception as e:
+    print(str(e))
+    print("Google Sheets pipeline failed")
+
+print("Success")
