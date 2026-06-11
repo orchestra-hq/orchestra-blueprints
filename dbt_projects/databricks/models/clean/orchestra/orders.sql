562b42a7e362a0e820320e1b589220d11e65cbee
gthesheep-orchestra <gary@getorchestra.io>
2026-06-11 17:06:40 +0100

    f

--- dbt_projects/databricks/models/clean/orchestra/orders.sql
+++ dbt_projects/databricks/models/clean/orchestra/orders.sql
@@ -2,5 +2,5 @@ select
     a.*,
     sha2(cast(a.order_id as string), 256) as _pk
 from {{ ref('orders_raw') }} a
-cross join range(100000) r
+cross join range(10000000) r
 qualify row_number() over (partition by a.order_id order by r.id) = 1
